405a2545bd1976bde7d3b35980085667c5a306ea
Julio C. Rocha <jurocha@microsoft.com>
2022-12-15 19:18:03 -0800

    Define ListenAsync

--- Program.cs
+++ Program.cs
@@ -3,36 +3,34 @@
 
 using System.Net;
 
-
-if (!HttpListener.IsSupported)
+async Task ListenAsync(HttpListener listener)
 {
-	Console.WriteLine ("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
-	return;
+	var context = await listener.GetContextAsync();
+	var request = context.Request;
+
+	// Obtain a response object.
+	var response = context.Response;
+	// Construct a response.
+	string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+	byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+	// Get a response stream and write the response to it.
+	response.ContentLength64 = buffer.Length;
+	using (var output = response.OutputStream)
+	{
+		output.Write(buffer,0,buffer.Length);
+		// You must close the output stream.
+		output.Close();
+	}
 }
 
 // Create a listener.
 HttpListener listener = new HttpListener();
-
 // Add the prefixes.
 listener.Prefixes.Add("http://localhost:5555/");
-
 listener.Start();
+
 Console.WriteLine("Listening...");
-// Note: The GetContext method blocks while waiting for a request.
-HttpListenerContext context = listener.GetContext();
-HttpListenerRequest request = context.Request;
-// Obtain a response object.
-HttpListenerResponse response = context.Response;
-// Construct a response.
-string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-// Get a response stream and write the response to it.
-response.ContentLength64 = buffer.Length;
-using (System.IO.Stream output = response.OutputStream)
-{
-	output.Write(buffer,0,buffer.Length);
-	// You must close the output stream.
-	output.Close();
-}
+
+ListenAsync(listener).GetAwaiter().GetResult();
 
 listener.Stop();
